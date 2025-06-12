0229106e5a27bd56776955ad04d2ca16b272401e
AlejoVV <alejovalevidal@gmail.com>
2025-06-12 01:22:52 -0500

    feat: add automatic updater system with GitHub releases integration

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
 import TableOrdenesMaestra from "./components/TableOrdenesMaestra";
 import ObjetivosConPreciosTable from "./components/TableObjetivosPreciosSupabase";
 import Sidebar from "./components/Sidebar";
+import { UpdateNotification } from "./components/UpdateNotification";
 import { useState } from "react";
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
     <div className="flex flex-row h-screen w-screen">
       <Sidebar activeView={activeView} onViewChange={setActiveView} />
       <div className="flex-1 overflow-hidden">{renderContent()}</div>
+      <UpdateNotification />
     </div>
   );
 }
